9dad232e2b461d538de56f0ae3830c26767e7556
zDju <zdju@rocketmail.com>
2016-06-01 15:10:28 +0200

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -9,8 +9,6 @@ Game::Game(QWidget *parent){
     scene->setSceneRect(0,0,800,600); // make the scene 800x600 instead of infinity by infinity (default)
     setBackgroundBrush(QBrush(QImage(":/images/bg.jpg")));
 
-    // make the newly created scene the scene to visualize (since Game is a QGraphicsView Widget,
-    // it can be used to visualize scenes)
     setScene(scene);
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
@@ -18,7 +16,7 @@ Game::Game(QWidget *parent){
 
     // create the player
     player = new Player();
-    player->setPos(0,0); // TODO generalize to always be in the middle bottom of screen
+    player->setPos(0,0);
     // make the player focusable and set it to be the current focus
     player->setFlag(QGraphicsItem::ItemIsFocusable);
     player->setFocus();
