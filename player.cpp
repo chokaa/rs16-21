102a969b4b4186ebbc1b9fb850798bb1646f613e
zDju <zdju@rocketmail.com>
2016-06-03 20:18:33 +0200

    popravljena kolizija da radi samo sa fixedWall v3

--- player.cpp
+++ player.cpp
@@ -2,6 +2,9 @@
 #include <QGraphicsScene>
 #include <QKeyEvent>
 #include "bomb.h"
+#include <typeinfo>
+#include "fixedwall.h"
+#include <iostream>
 
 Player::Player(QGraphicsItem *parent): QGraphicsPixmapItem(parent){
     setPixmap(QPixmap(":/images/osama.jpg"));
@@ -12,18 +15,31 @@ void Player::keyPressEvent(QKeyEvent *event){
     if (event->key() == Qt::Key_Left){
         if (pos().x() > 0 && !scene()->itemAt(x()-50,y(), QTransform()))
             setPos(x()-50,y());
+        else if((scene()->itemAt(x()-50,y(), QTransform())))
+            if(!((typeid(*(scene()->itemAt(x()-50,y(), QTransform())))) == typeid(fixedWall)))
+                setPos(x()-50, y());
     }
     else if (event->key() == Qt::Key_Right){
-        if ((pos().x() + 100 <= 800 && !scene()->itemAt(x()+50,y(), QTransform())))
-            setPos(x()+50,y());
+        if (pos().x() + 100 <= 800 && !(scene()->itemAt(x()+50,y(), QTransform()))){
+                setPos(x()+50,y());
+        }
+        else if((scene()->itemAt(x()+50,y(), QTransform())))
+            if(!((typeid(*(scene()->itemAt(x()+50,y(), QTransform())))) == typeid(fixedWall)))
+                setPos(x()+50, y());
     }
     else if (event->key() == Qt::Key_Up){
         if (pos().y() > 0 && !scene()->itemAt(x(),y()-50, QTransform()))
             setPos(x(),y()-50);
+        else if((scene()->itemAt(x(),y()-50, QTransform())))
+            if(!((typeid(*(scene()->itemAt(x(),y()-50, QTransform())))) == typeid(fixedWall)))
+                setPos(x(), y()-50);
     }
     else if (event->key() == Qt::Key_Down){
         if (pos().y() + 100 <= 600 && !scene()->itemAt(x(),y()+50, QTransform()))
             setPos(x(),y()+50);
+        else if((scene()->itemAt(x(),y()+50, QTransform())))
+            if(!((typeid(*(scene()->itemAt(x(),y()+50, QTransform())))) == typeid(fixedWall)))
+                setPos(x(), y()+50);
     }
 
     // place bomb with the spacebar
@@ -33,5 +49,4 @@ void Player::keyPressEvent(QKeyEvent *event){
         bomb->setPos(x(),y());
         scene()->addItem(bomb);
     }
-
 }
